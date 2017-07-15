cf47a50bd47a4b3fdc4776091db5396f47495c02
jerry2714 <jerry2714@gmail.com>
2017-07-15 19:45:37 +0800

    bug fix

--- musicprocessor/processor/SpectrumStrategy.java
+++ musicprocessor/processor/SpectrumStrategy.java
@@ -4,9 +4,8 @@ package ledcubeproject.models.musicprocessor.processor;
  * 配合SimpleSpectrumAnalyzer，目的在於提供SimpleSpectrumAnalyzer可替換的輸出圖形產生方法
  * Created by Jerry on 2017/7/10.
  */
-interface SpectrumStrategy {
-    //static final int ranges[] = {0, 200, 400, 800, 1600, 3200, 6400};
-    static final int amount = 250;
+public interface SpectrumStrategy {
+
 
     /**
      * 處理頻譜，產生想要的圖形
